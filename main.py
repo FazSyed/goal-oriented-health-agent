2614a197f8be3743ffba88def476abaae52c14f7
FazSyed <fazsyed1@gmail.com>
2025-08-03 18:48:03 +0400

    imported asyncio

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import asyncio
 import agents.main_controller as mc
 
 # This is the main entry point for the agent system.
 # It initializes and starts the agents defined in the system.
 # The agents will communicate with each other to perform their tasks.
 if __name__ == "__main__":
-    mc.main()
+    asyncio.run(mc.main())
