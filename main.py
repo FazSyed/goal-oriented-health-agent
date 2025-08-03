2c0977ad989828ad4f740be483154aba983a048a
FazSyed <fazsyed1@gmail.com>
2025-08-03 16:56:49 +0400

    created code to run project

---
+++ main.py
@@ -0,0 +1,7 @@
+import agents.main_controller as mc
+
+# This is the main entry point for the agent system.
+# It initializes and starts the agents defined in the system.
+# The agents will communicate with each other to perform their tasks.
+if __name__ == "__main__":
+    mc.main()
